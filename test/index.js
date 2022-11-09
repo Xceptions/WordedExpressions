afb99f8042c172560a9bc8e9b57e971c62cf0880
Xceptions <agbokenechukwu.k@gmail.com>
2022-11-09 23:01:31 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -34,10 +34,3 @@ describe('Test query: FIND', () => {
         done();
     });
 });
-
-
-
-// RegexQuery("FIND a IN happy") // returns a
-// RegexQuery("REPLACE a WITH b IN happy") // returns hbppy
-// RegexQuery("IS p IN happy") // returns yes, otherwise returns no
-// RegexQuery("COUNT p IN happy") // returns 2
